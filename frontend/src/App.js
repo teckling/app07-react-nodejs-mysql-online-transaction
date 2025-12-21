5eeacc143570bb0cea32a0a48991d00b963e8ef2
teckling <teckling.tan@yahoo.com>
2025-12-21 22:00:29 +0800

    adding ServiceReportComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ServiceReportComponent from "./ServiceReportComponent";
 
 function App() {
+  const instruction = <div>This is an instruction const</div>;
   return (
     <div className="App">
       <header className="App-header">
+        {instruction}
+        <ServiceReportComponent />
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ frontend/src/ServiceReportComponent.js
@@ -0,0 +1,9 @@
+import "./ServiceReportComponent.css";
+
+function ServiceReportComponent() {
+  return (
+    <div id="service-report">This is a Service Report React Component</div>
+  );
+}
+
+export default ServiceReportComponent;
