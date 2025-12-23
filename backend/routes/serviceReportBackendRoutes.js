b2e3ba2a44b72d3e48d31c7b4401f3655a5154ac
teckling <teckling.tan@yahoo.com>
2025-12-23 23:23:48 +0800

    insert a row into database table

--- backend/routes/serviceReportBackendRoutes.js
+++ backend/routes/serviceReportBackendRoutes.js
@@ -9,7 +9,21 @@ router.get("/", function (req, res, next) {
   const conn = mysql.createConnection(db);
   conn.connect((err) => {
     if (err) throw err;
-    console.log("connected to database");
+
+    const dummyReport = {
+      customer_name: "John",
+      service_summary:
+        "Replace capacitor in power supply unit of Lenovo notebook ",
+      service_completion: "1968-01-02",
+    };
+    conn.query(
+      "insert into service_reports set ?",
+      dummyReport,
+      (err, result) => {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
   });
   res.send("This route is supposed to send a listing of service reports");
 });
