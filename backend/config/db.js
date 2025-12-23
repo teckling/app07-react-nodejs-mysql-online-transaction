f06448bac436176cb0b09eeac6ade871de9e1d94
teckling <teckling.tan@yahoo.com>
2025-12-23 18:39:25 +0800

    change mysql to mysql2

---
+++ backend/config/db.js
@@ -0,0 +1,8 @@
+const db = {
+  user: "admin",
+  password: "admin",
+  host: "localhost",
+  database: "customer_service_db",
+};
+
+module.exports = db;
--- backend/routes/serviceReportBackendRoutes.js
+++ backend/routes/serviceReportBackendRoutes.js
@@ -1,8 +1,16 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const mysql = require("mysql2");
+const router = express.Router();
+
+const db = require("../config/db");
 
 /* GET service reports listing. */
 router.get("/", function (req, res, next) {
+  const conn = mysql.createConnection(db);
+  conn.connect((err) => {
+    if (err) throw err;
+    console.log("connected to database");
+  });
   res.send("This route is supposed to send a listing of service reports");
 });
 
